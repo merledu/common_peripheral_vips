59324a9629cc3180fc920d954623944144122a51
Auringzaib Sabir <auringzaibsabir11@gmail.com>
2022-01-19 12:39:52 +0500

    Added a target in Makefile

--- verif/vips/timer/script.py
+++ verif/vips/timer/script.py
@@ -1,5 +1,13 @@
+# for x in range(5):
+#    print(os.environ.get("SEED"))
+#    print(os.environ.get("OUT"))
+#    print(os.environ.get("OUT-SEED"))
+#    print(os.environ.get("GEN_OPTS"))
 # importing os module
+
 import os
+import shutil
+import random
 
 # Setting relative path (__file__)
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ""))
@@ -11,54 +19,61 @@ def make_directory(make_dir):
     parent_dir = ROOT_DIR
     # Directory to create
     directory = make_dir
-    # Complete of directory to be created
-    path = os.path.join(parent_dir, directory)
-    print("path=", path)
-    # Check if directory already exist
-    is_dir = os.path.isdir(path)
-    print(is_dir)
     # Create the directory
-
     if make_dir == os.environ.get("OUT"):
+        # Complete of directory to be created
+        path = os.path.join(parent_dir, directory)
+        print("path=", path)
+        # Check if directory already exist
+        is_dir = os.path.isdir(path)
+        print(is_dir)
         if is_dir == False:
-            print("Directory '% s' created" % directory)
+            print("Directory for output '% s' created" % directory)
             os.mkdir(path)
         else:
-            print("Directory '% s' already exist" % directory)
-    else
+            print("Directory for output '% s' already exist" % directory)
+    else:
+        # Complete of directory to be created
+        # path = parent_dir + "/out/" + directory
+        path = os.path.join(parent_dir, directory)
+        print("path=", path)
+        # Check if directory already exist
+        is_dir = os.path.isdir(path)
+        print(is_dir)
+
         if is_dir == False:
-            print("Directory '% s' created" % directory)
-            os.mkdir(path)
+            print("Directory for Seed '% s' created" % directory)
+            os.makedirs(path)
         else:
-            print("Directory '% s' already exist" % directory)
+            print("Directory for Seed '% s' already exist" % directory)
+            shutil.rmtree(path)
 
-# def make_directory (args):
-#    # Parent Directory path
-#    parent_dir = ROOT_DIR
-#    # Directory to create
-#    directory = os.environ.get("OUT")
-#    # Complete of directory to be created
-#    path = os.path.join(parent_dir, directory)
-#    print("path=", path)
-#    # Check if directory already exist
-#    is_dir = os.path.isdir(path)
-#    print(is_dir)
-#    # Create the directory
-#    if is_dir == False:
-#        print("Directory '% s' created" % directory)
-#        os.mkdir(path)
-#    else:
-#        print("Directory '% s' already exist" % directory)
 
-# for x in range(5):
-#    print(os.environ.get("SEED"))
-#    print(os.environ.get("OUT"))
-#    print(os.environ.get("OUT-SEED"))
-#    print(os.environ.get("GEN_OPTS"))
+def run_test():
+    cmd = (
+        "xrun -clean -sv -uvm -access rwc -linedebug -f ./compile_file/run.f -svseed "
+        + os.environ.get("SEED")
+        + " +UVM_TESTNAME=tx_test +UVM_VERBOSITY=UVM_LOW +define+UVM_REPORT_DISABLE_FILE_LINE +uvm_set_config_int=uvm_test_top,base_address,23 +UVM_CONFIG_DB_TRACE +UVM_OBJECTION_TRACE"
+    )
+    print("Command to run = ", cmd)
+    os.system(cmd)
+
+
+def run_make():
+    cmd = "make run_py"
+    print("Command to run = ", cmd)
+    os.system(cmd)
 
 
 def main():
-    make_directory(os.environ.get("OUT"))
+    # make_directory(os.environ.get("OUT"))
+    # make_directory(os.environ.get("OUT-SEED"))
+    num = 0
+    if num < 2:
+        num = num + 1
+        print("number= ", num)
+        run_test()
+        run_make()
 
 
 if __name__ == "__main__":
