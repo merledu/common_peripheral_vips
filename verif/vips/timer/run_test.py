4d310d70a02559678c5a0519a530d8329eca999c
Auringzaib Sabir <auringzaibsabir11@gmail.com>
2022-01-21 13:32:27 +0500

    Incorporated code to set the number of tests to run, its a run time argument passed by command line

--- verif/vips/timer/run_test.py
+++ verif/vips/timer/run_test.py
@@ -11,5 +11,10 @@ def run_test():
 
 
 if __name__ == "__main__":
-    for x in range(2):
+    # This is run-time argument, which can be set from command line to instruct how many tests to run
+    no_of_tests = sys.argv
+    # Converting this list object to integer
+    num_of_tests = int(no_of_tests[1])
+    print("number of test to run = ", num_of_tests)
+    for x in range(num_of_tests):
         run_test()
