17f027d5541e07c1b0829e0165a666f18fb5ad7c
Auringzaib Sabir <auringzaibsabir11@gmail.com>
2022-05-26 14:30:39 +0500

    Configuring Control, divider,Tx registers and also removed unnecessary code

--- verif/vips/spi/sequences/config_spi_sequence.sv
+++ verif/vips/spi/sequences/config_spi_sequence.sv
@@ -91,7 +91,7 @@ class config_spi_sequence extends uvm_sequence #(transaction_item);
     string msg="";
 
     // config_spi_sequence is going to generate 4 transactions of type transaction_item
-    repeat(4) begin
+    repeat(5) begin
       cycle = cycle + 1;
       tx = transaction_item::type_id::create("tx");              // Factory creation (body task create transactions using factory creation)
       start_item(tx);                                            // Waits for a driver to be ready
@@ -103,7 +103,7 @@ class config_spi_sequence extends uvm_sequence #(transaction_item);
       tx.rst_ni = 1'b1;        
 
       //Configuring Control and status register
-      // Assigning values TODO: Add fields for tx and rx enables
+      // Assigning values
       if (cycle == 'd1) begin
         reserved_2 = 18'd0;
         rx_en      = 1'b0;
@@ -126,130 +126,70 @@ class config_spi_sequence extends uvm_sequence #(transaction_item);
         tx.we_i    = 1;       
         tx.re_i    = 0;        
         //tx.sd_i    = ;    // miso
+        print_transaction(tx, "Configuring Control Register");
       end 
       // Configuring TX register
       else if (cycle == 'd2) begin
-        //tx.addr_i  = ;            
+        tx.addr_i  = 'h0;            
         //tx.wdata_i = ;              
-        //tx.be_i    = ;           
-        tx.we_i    = 1;       
-        tx.re_i    = 0;        
+        tx.be_i    = 'h0;           
+        tx.we_i    = 'h1;       
+        tx.re_i    = 'h0;        
         //tx.sd_i    = ;
+        print_transaction(tx, "Configuring TX Register");
       end
-      // Configuring Divider
+      // Configuring the Divider
       else if (cycle == 'd3) begin
         tx.addr_i  = 'h14;            
         tx.wdata_i = 'h0;              
-        //tx.be_i    = ;           
+        tx.be_i    = 'h0;           
+        tx.we_i    = 1'h1;       
+        tx.re_i    = 1'h0;        
+        //tx.sd_i    = ;
+        print_transaction(tx, "Configuring Divider");
+      end
+      // Selecting Slave of 4 slave 
+      else if (cycle == 'd4) begin
+        tx.addr_i  = 'h18;            
+        tx.wdata_i = 'h0;              
+        tx.be_i    = 'h0;           
+        tx.we_i    = 1'h1;       
+        tx.re_i    = 1'h0;        
+        //tx.sd_i    = ;
+        print_transaction(tx, "Selecting slave");
+      end
+      // Enabling Transmition
+      else if (cycle == 'd5) begin
+        tx_en      = 1'b1;
+        go_bsy     = 1'b1;
+        ctrl_reg = {reserved_2,rx_en,tx_en,ass,ie,lsb,tx_neg,rx_neg,go_bsy,reserved_1,char_len};
+        `uvm_info ("CONFIG_SPI_SEQUENCES::", $sformatf("ctrl_reg = %0b", ctrl_reg), UVM_LOW)
+        // transaction
+        tx.addr_i  = 'h10;           
+        tx.wdata_i = ctrl_reg;              
+        tx.be_i    = 0;           
         tx.we_i    = 1;       
         tx.re_i    = 0;        
-        //tx.sd_i    = ;
+        //tx.sd_i    = ;    // miso
+        print_transaction(tx, "Enabing transmission from master");
       end
-
-      //// Declaration and initialization
-      //tx.rst_ni = 1'b1;
-      //tx.reg_we = 1'h1;
-      //tx.reg_be = 4'b1111;
-      //tx.reg_re = 1'h0;
-      //
-      //if (cycle == 'b01 || cycle =='b10) begin
-      //  //tx.reg_wdata = 64'h000000F0FFFFFFFF;
-      //  `uvm_info("\nconfig_spi_sequence::",$sformatf(" [CHECK DATA___________:: %0b", tx.reg_wdata), UVM_LOW)
-      //  // If the data is less than or equal to 64'h00000000FFFFFFFF then put the lower 32bit of data in register at address 'h10c 
-      //  if(tx.reg_wdata <= 64'h00000000FFFFFFFF && cycle == 'b1) begin
-      //    tx.reg_addr = 'h10c;
-      //    data = tx.reg_wdata;
-      //    print_transaction(tx, "Value to be counted is less than 32 bit");
-      //  end
-      //  // If the data is less than or equal to 64'h00000000FFFFFFFF then set 32bit register at address 'h110 to zero 
-      //  else if (cycle == 'b10 && data <= 64'h00000000FFFFFFFF && lower_data_en == 1'b0) begin
-      //    tx.reg_addr = 'h110;
-      //    tx.reg_wdata = 64'h00000000;
-      //    print_transaction(tx, "Setting all bit of upper compare register to zero");
-      //  end
-      //  // If the data is greater than 64'h00000000FFFFFFFF then put the lower 32bit of data in register at address 'h10c 
-      //  else if (lower_data_en == 1) begin
-      //    tx.reg_addr = 'h10c;
-      //    tx.reg_wdata = {32'h00000000 , lower_data};
-      //    lower_data_en = 1'b0;
-      //    print_transaction(tx, "Setting up the lower compare registere as lower_data(lower 32 bit) of input data");
-      //  end
-      //  // If the data is greater than 64'h00000000FFFFFFFF then put the upper 32bit of data in register at address 'h110 
-      //  else begin
-      //    upper_data = tx.reg_wdata[63:32];
-      //    lower_data = tx.reg_wdata[31:0 ];
-      //    tx.reg_addr = 'h110;
-      //    tx.reg_wdata = {32'h00000000 , upper_data};
-      //    lower_data_en = 1'b1; // Enable bit to assign lower bit to respective register i.e. to tx.reg_wdata[31:0 ] at address 0x110
-      //    print_transctn_data(tx, "Value to be counted is greater than 32 bit", upper_data, lower_data);
-      //  end
-      //end
-      //// Configure the timer by writing random values in register at address 'h100 for setting prescale and step value of timer
-      //else if (cycle == 'b11) begin
-      //  tx.reg_addr = 'h100;
-      //  prescale =  /*4*/$urandom();
-      //  step     =  /*6*/$urandom();
-      //  tx.reg_wdata = {8'b11111111, step, 4'b1111, prescale};
-      //  if(step == 0)
-      //    `uvm_fatal("config_spi_sequence::FATAL ERROR",$sformatf("Step value is set to zero, please re-run the test"))
-      //  //msg = "";
-      //  //$sformat(msg, {2{"%s============================"}}    , msg                     );
-      //  //$sformat(msg, "%s\nPRE-SCALE RANDOM___________:: %0b"  , msg, prescale           );
-      //  //$sformat(msg, "%s\nASSIGNED PRE-SCALE_________:: %0b"  , msg, tx.reg_wdata[11:0] );
-      //  //$sformat(msg, "%s\nSTEP RANDOM________________:: %0b"  , msg, step               );
-      //  //$sformat(msg, "%s\nASSIGNED STEP______________:: %0b"  , msg, tx.reg_wdata[23:16]);
-      //  //$sformat(msg, "%s\nWDATA______________________:: %0b\n", msg, tx.reg_wdata       );
-      //  //$sformat(msg, {2{"%s============================"}}    , msg                     );
-      //  //`uvm_info("config_spi_sequence::",$sformatf("\n\nSetting prescale and step for the counter\n", msg), UVM_LOW)
-      //  print_step_scale(tx, "Setting prescale and step for the counter", prescale, step);
-      //end
-      //// Enable the interupt pin by writing 1 in register at address 'h114
-      //else if (cycle == 'b100) begin
-      //  tx.reg_we    = 1'h1;
-      //  tx.reg_be    = 4'b1111;
-      //  tx.reg_addr  = 'h114;
-      //  tx.reg_wdata = 32'h00000001;
-      //  print_transaction(tx, "Enabling the interupt");
-      //end
 
       finish_item(tx);  // After randommize send it to the driver and waits for the response from driver to know when the driver is ready again to generate and send the new transaction and so on.
     end
   endtask // body
 
-  //function void print_transaction(transaction_item tx, input string msg);
-  //  $sformat(msg, {1{"\n%s\n========================================="}}, msg              );
-  //  $sformat(msg, "%s\nADDRESS__________:: %0h"                         , msg, tx.reg_addr );
-  //  $sformat(msg, "%s\nWRITE_EN_________:: %0b"                         , msg, tx.reg_we   );
-  //  $sformat(msg, "%s\nBYTE_EN__________:: %0b"                         , msg, tx.reg_be   );
-  //  $sformat(msg, "%s\nDATA_____________:: %0b\n"                       , msg, tx.reg_wdata);
-  //  $sformat(msg, {1{"%s=========================================\n"}}  , msg              );
-  //  `uvm_info("config_spi_sequence::",$sformatf("\n", msg), UVM_LOW)  
-  //  msg = "";
-  //endfunction : print_transaction
-  //
-  //function void print_transctn_data(transaction_item tx, input string msg, input bit [31:0] upper_data, input bit [31:0] lower_data);
-  //  $sformat(msg, {1{"\n%s\n========================================="}}, msg              );
-  //  $sformat(msg, "%s\nUPPER-DATA_______:: %0b"                         , msg, upper_data  );
-  //  $sformat(msg, "%s\nLOWER-DATA_______:: %0b"                         , msg, lower_data  );
-  //  $sformat(msg, "%s\nADDRESS__________:: %0h"                         , msg, tx.reg_addr );
-  //  $sformat(msg, "%s\nWRITE_EN_________:: %0b"                         , msg, tx.reg_we   );
-  //  $sformat(msg, "%s\nBYTE_EN__________:: %0b"                         , msg, tx.reg_be   );
-  //  $sformat(msg, "%s\nDATA_____________:: %0b\n"                       , msg, tx.reg_wdata);
-  //  $sformat(msg, {1{"%s========================================="}}    , msg              );
-  //  `uvm_info("config_spi_sequence::",$sformatf("\n", msg), UVM_LOW)  
-  //  msg = "";
-  //endfunction : print_transctn_data
-  //
-  //function void print_step_scale(transaction_item tx, input string msg, input bit [11:0] prescale, input bit [23:16] step);
-  //  $sformat(msg, {1{"\n%s\n========================================="}}, msg                     );
-  //  $sformat(msg, "%s\nPRE-SCALE RANDOM___________:: %0d"               , msg, prescale           );
-  //  $sformat(msg, "%s\nASSIGNED PRE-SCALE_________:: %0d"               , msg, tx.reg_wdata[11:0] );
-  //  $sformat(msg, "%s\nSTEP RANDOM________________:: %0d"               , msg, step               );
-  //  $sformat(msg, "%s\nASSIGNED STEP______________:: %0d"               , msg, tx.reg_wdata[23:16]);
-  //  $sformat(msg, "%s\nWDATA______________________:: %0b\n"             , msg, tx.reg_wdata       );
-  //  $sformat(msg, {1{"%s========================================="}}    , msg                     );
-  //  `uvm_info("config_spi_sequence::",$sformatf("\n", msg), UVM_LOW)
-  //  msg = "";
-  //endfunction : print_step_scale
+  function void print_transaction(transaction_item tx, input string msg);
+    $sformat(msg, {1{"\n%s\n========================================="}}, msg            );
+    $sformat(msg, "%s\nRESET__________:: %0h"                           , msg, tx.rst_ni );
+    $sformat(msg, "%s\nADDRESS________:: %0b"                           , msg, tx.addr_i );
+    $sformat(msg, "%s\nDATA___________:: %0b"                           , msg, tx.wdata_i);
+    $sformat(msg, "%s\nBYTE_EN________:: %0b"                           , msg, tx.be_i   );
+    $sformat(msg, "%s\nWRITE_EN_______:: %0b"                           , msg, tx.we_i   );
+    $sformat(msg, "%s\nREAD_EN________:: %0b"                           , msg, tx.re_i   );
+    $sformat(msg, "%s\nMISO___________:: %0b\n"                         , msg, tx.sd_i   );
+    $sformat(msg, {1{"%s======================================== \n"}}  , msg            );
+    `uvm_info("CONFIG_SPI_SEQUENCES::",$sformatf("\n", msg), UVM_LOW)
+    msg = "";
+  endfunction : print_transaction        
 
 endclass // config_spi_sequence
